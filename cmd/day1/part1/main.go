423afa2e2e5db56fe146323c6f8406538ae6ae46
Brian Walheim <bwalheim1205@vt.edu>
2025-12-03 19:40:52 -0500

    Day 1 commit

---
+++ cmd/day1/part1/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input_01.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create scanner to read file
+	scanner := bufio.NewScanner(file)
+
+	dialValue := 50
+	zeroCount := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		var direction string = line[0:1]
+		value, _ := strconv.Atoi(line[1:])
+
+		switch direction {
+		case "R":
+			dialValue += value
+		case "L":
+			dialValue -= value
+		}
+
+		// If negative, increase by 100 means pased 0
+		for dialValue < 0 {
+			dialValue += 100
+		}
+
+		dialValue = dialValue % 100
+
+		if dialValue == 0 {
+			zeroCount++
+		}
+	}
+
+	fmt.Println(zeroCount)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bwalheim1205/advent-2025
+
+go 1.22.2
